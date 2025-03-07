2dc274f1be79a7ffe5b374a761b41f01fa8a0eef
quentalt <quentin.altieri@gmail.com>
2025-03-07 14:26:30 +0100

    fix +server.ts

--- src/routes/api/tags/+server.ts
+++ src/routes/api/tags/+server.ts
@@ -4,5 +4,5 @@ import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async () => {
     const tags = await getAllTags();
-    return json(tags);
-};
+    return json({ tags });
+}
